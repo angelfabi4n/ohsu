c3fbe8be29399b69c0303d15c7efbd79d5d21309
angelfabi4n <afalvaradov13@gmail.com>
2025-07-15 16:47:47 -0700

    We filtered genes using the limma package and obtained the desired dataset, which will be used for further biological analysis and interpretation.

--- Limma Script.R
+++ Limma Script.R
@@ -1,8 +1,9 @@
 library(limma)
 library(tidyverse)
 
-#Input File
-inputFile <- "Custom/GSE200431_all_dep_ctrl_noNA.csv"
+########################################### Put path to input file here
+inputFile <- "csv-dataset/GSE200431_all_dep_ctrl_noNA.csv"
+###########################################
 
 # Import log2CPM data into R
 log2CPM_matrix <- read.csv(inputFile)  
@@ -18,26 +19,42 @@ log2CPM_matrix <-
 # Create a vector of sample names
 whichSamples <- colnames(log2CPM_matrix)  
 
-# Use brackets to subset samples to analyze
-whichSamples <- whichSamples[-c(1:12, 19:24)]
+########################################### Use brackets to subset samples to analyze
+whichSamples <- whichSamples[-c(1:12, 19:24)]  
+
+###########################################
 
 # Extract treatment names from group names
 group <- str_remove((str_extract(whichSamples, "[_][A-Za-z]+")), "_")
 group <- as.factor(group)                                          
 
-# Create design matrix for linear model 
+########################################### # Create design matrix for linear model 
 design <- model.matrix(~ 0 + group)
+########################################### 
 colnames(design) <- levels(group)
 design
 
 # Fit linear model to log2CPM data
 fit <- lmFit(log2CPM_matrix[, whichSamples], design)
 
-# Define contrast(s), e.g., treated vs control
+########################################### Define contrast(s), e.g., treated vs control
 contrast.matrix <- makeContrasts(CntrlvsSH= plko - shPTN,levels=design)
+###########################################
+
 fit2 <- contrasts.fit(fit, contrast.matrix)
 fit2 <- eBayes(fit2)
 
 # Extract results
 topTable(fit2, coef="CntrlvsSH", lfc = 0, number = 20)
+
+# Create new file
+cooltable <- topTable(fit2, coef="CntrlvsSH", lfc = 0, number = 20)
+
+# Filter tale to select genes with adjusted p-value < 0.5 & log2 fold change > 1 or < -1
+library(dplyr)
+coolfilteredgenes <- cooltable %>%
+  filter(adj.P.Val < 0.5 & (logFC > 1 | logFC < -1))
+
+# Write table to CSV file
+write.csv(coolfilteredgenes, "GSEFiltered_PTN_Signature.csv", row.names = FALSE)
 
